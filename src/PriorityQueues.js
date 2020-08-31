71ca6ebaec4ec2807fbeea5f4c69053efd9f389e
Salman <salman.khan@coutallure.com>
2020-08-31 11:56:57 +0100

    priority queue poll logic completed

--- src/PriorityQueues.js
+++ src/PriorityQueues.js
@@ -4,11 +4,11 @@ class PriorityQueue {
   }
 
   getLeftChildNodeIndex(i) {
-    return 2(i) + 1;
+    return 2 * i + 1;
   }
 
   getRightChildNodeIndex(i) {
-    return 2(i) + 2;
+    return 2 * i + 2;
   }
 
   getParentNodeIndex(i) {
@@ -33,28 +33,66 @@ class PriorityQueue {
     }
   }
 
-  // removing a node is called polling
-  poll() {}
+  // removal of highest priority node (index=0) is called polling
+  poll() {
+    const { queue } = this;
+    // remove the top priority value at index 0
+    const topPriority = queue[0];
+    // new queue has bottom value at top & we shrink the size
+    // of the queue as there is one less value
+    queue[0] = queue.pop();
+    console.log("queue:::", queue);
+    let replacedValIndex = 0;
+    let replacedVal = queue[replacedValIndex];
+    let leftChildNodeIndex = this.getLeftChildNodeIndex(replacedValIndex);
+    let leftChildNodeVal = queue[leftChildNodeIndex];
+    console.log(
+      ":::",
+      replacedVal,
+      replacedValIndex,
+      leftChildNodeVal,
+      leftChildNodeIndex
+    );
+    while (replacedVal > leftChildNodeVal) {
+      queue[replacedValIndex] = leftChildNodeVal;
+      queue[leftChildNodeIndex] = replacedVal;
+      replacedValIndex = leftChildNodeIndex;
+      leftChildNodeIndex = this.getLeftChildNodeIndex(leftChildNodeIndex);
+      leftChildNodeVal = queue[leftChildNodeIndex];
+      console.log(
+        "new ::::",
+        replacedVal,
+        replacedValIndex,
+        leftChildNodeVal,
+        leftChildNodeIndex
+      );
+    }
+    console.log("Poll extracted:", topPriority);
+  }
+
+  remove() {}
 
   find() {}
 }
 
 // TESTS //
 const pq1 = new PriorityQueue();
-console.log("=======queue", pq1);
-console.log("--------About to insert: 3");
+// console.log("=======queue", pq1);
+// console.log("--------About to insert: 3");
 pq1.insert(3);
-console.log("=======queue", pq1);
-console.log("--------About to insert: 10");
+// console.log("=======queue", pq1);
+// console.log("--------About to insert: 10");
 pq1.insert(10);
-console.log("=======queue", pq1);
-console.log("--------About to insert: 23");
+// console.log("=======queue", pq1);
+// console.log("--------About to insert: 23");
 pq1.insert(23);
-console.log("=======queue", pq1);
-console.log("--------About to insert: 7");
+// console.log("=======queue", pq1);
+// console.log("--------About to insert: 7");
 pq1.insert(7);
-console.log("=======queue", pq1);
+// console.log("=======queue", pq1);
 pq1.insert(60);
-console.log("=======queue", pq1);
+// console.log("=======queue", pq1);
 pq1.insert(2);
+console.log("=======queue", pq1);
+pq1.poll();
 console.log("=======queue", pq1);
